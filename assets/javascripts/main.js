b17043a31d96a769e7385d05d717b3e368cf75fb
lacostenycoder <djlancejordan@gmail.com>
2014-10-18 16:10:43 -0400

    added gallery with pop-up images

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,13 +1,59 @@
 $(document).ready(function(){
  $('li img').on('click',function(){
   var src = $(this).attr('src');
-  var img = '<img src="' + src + '" class="img-responsive"/>';
+  var img = '<img src="' + src + '" class="img-responsive center-block gallery-popup"/>';
+
+  //Start of new code
+  var index = $(this).parent('li').index();
+  var html = '';
+  html += img;
+  // html += '<div style="height:25px;">';
+  html += '<a class="controls next btn btn-default clear-left" href="'+ (index+2) + '">next</a>';
+  html += '<button type="button" class="btn btn-default center-block" data-dismiss="modal">Close</button>';
+  html += '<a class="controls previous btn btn-default left-block" href="' + (index) + '">prev</a>';
+  html += '</div>';
+  //End of new code
+
   $('#myModal').modal();
   $('#myModal').on('shown.bs.modal', function(){
-    $('#myModal .modal-body').html(img);
+    $('#myModal .modal-body').html(html);
+    //this will hide or show the right links:
+    $('a.controls').trigger('click');
   });
   $('#myModal').on('hidden.bs.modal', function(){
     $('#myModal .modal-body').html('');
   });
  });
 })
+
+$(document).on('click', 'a.controls', function(){
+   //this is where we add our logic
+   var index = $(this).attr('href');
+   var src = $('ul.row li:nth-child('+ index +') img').attr('src');
+   $('.modal-body img').attr('src', src);
+   var newPrevIndex = parseInt(index) - 1;
+   var newNextIndex = parseInt(newPrevIndex) + 2;
+
+  if($(this).hasClass('previous')){
+      $(this).attr('href', newPrevIndex);
+      $('a.next').attr('href', newNextIndex);
+  }else{
+      $(this).attr('href', newNextIndex);
+      $('a.previous').attr('href', newPrevIndex);
+  }
+  var total = $('ul.row li').length + 1;
+  //hide next button
+  if(total === newNextIndex){
+      $('a.next').hide();
+  }else{
+      $('a.next').show()
+  }
+  //hide previous button
+  if(newPrevIndex === 0){
+      $('a.previous').hide();
+  }else{
+      $('a.previous').show()
+  }
+
+   return false;
+});
